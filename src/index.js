0e5a71369e89f74b66d6f27706df5e369c01c903
EFEX <nicolasm@hotmail.es>
2025-03-29 16:53:04 -0300

    chore: import global css

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './ui/styles/components.css';
+import './styles/global.css';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
